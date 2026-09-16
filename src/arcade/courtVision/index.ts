02ace55fd7cceb1140b7d2a66f6b0a70bd3709e1
zenotheomni <fiftry5@gmail.com>
2026-09-16 09:12:07 -0700

    feat(court-vision): pointer handlers + barrel export

--- src/arcade/courtVision/index.ts
+++ src/arcade/courtVision/index.ts
@@ -1,2 +1,3 @@
 export { default as CourtVisionEndless } from './CourtVisionEndless'
 export { scoreShot, streakMultiplier } from './scoring'
+export { useCourtVisionGame } from './useCourtVisionGame'
